0c1eb6df436ce0859f7d4a174c3dc67a1361501c
giaphoang <hhgiap241>
2022-06-20 16:17:08 +0700

    add permission base authentication

--- src/main/java/com/kms/mywebapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/kms/mywebapp/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.kms.mywebapp.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.kms.mywebapp.security.ApplicationUserPermission.*;
 import static com.kms.mywebapp.security.ApplicationUserRole.*;
 
 /**
@@ -37,7 +39,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/index", "/static/**", "/webjars/**").permitAll()
-                .antMatchers("/**").hasRole(ADMIN.name())
+//                .antMatchers("/students/add", "/students/edit/**", "/students/delete/**", "/books/add", "/books/edit/**", "/books/delete/**","/courses/add", "/courses/edit/**", "/courses/delete/**" )
+//                .hasRole(ADMIN.name())
+                .antMatchers("/students/add/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers("/books/add/**").hasAuthority(BOOK_WRITE.getPermission())
+                .antMatchers("/courses/add/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/students/edit/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers("/students/delete/**").hasAuthority(STUDENT_WRITE.getPermission())
+                .antMatchers("/books/edit/**").hasAuthority(BOOK_WRITE.getPermission())
+                .antMatchers("/books/delete/**").hasAuthority(BOOK_WRITE.getPermission())
+                .antMatchers("/courses/edit/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/courses/delete/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/students/*", "/books/*", "/courses/*").hasAnyRole(TEACHER.name(), ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -49,21 +62,26 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails teacherUser = User.builder()
                 .username("teacher")
                 .password(passwordEncoder.encode("teacher"))
-                .roles(TEACHER.name())
+//                .roles(TEACHER.name())
+                .authorities(TEACHER.getGrantedAuthorities())
                 .build();
 
         UserDetails adminUser = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails adminTraineeUser = User.builder()
                 .username("admin_trainee")
                 .password(passwordEncoder.encode("admin_trainee"))
-                .roles(ADMINTRAINEE.name())
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(teacherUser, adminUser, adminTraineeUser);
     }
 }
+// role base authorization
+// authorization based on user permission
